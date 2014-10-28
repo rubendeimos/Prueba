16380c51780cf3f1ed7b3b524922ff5f56dabea5
usuario <rubenclase12@gmail.com>
2014-10-28 21:38:23 +0100

    Punto de partida

---
+++ HolaMundo/src/controlador/Principal.java
@@ -0,0 +1,17 @@
+package controlador;
+
+import modelo.Saludar;
+
+public class Principal {
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		Saludar saludo=new Saludar();
+		saludo.diHola(" Ruben");
+
+	}
+
+}
---
+++ HolaMundo/src/modelo/Saludar.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class Saludar {
+
+	public Saludar() {
+		// TODO Auto-generated constructor stub
+	}
+	public void diHola(String nombre){
+		System.out.println("Hola" + nombre + " que tal...");
+	}
+}
